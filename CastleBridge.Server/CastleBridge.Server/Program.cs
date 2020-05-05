3cdb4f43b6c70ee7e38603c988f13c2f33c19939
Idan Bachar <idan_bachar@hotmail.com>
2020-05-05 18:46:30 +0300

    initialize

---
+++ CastleBridge.Server/CastleBridge.Server/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CastleBridge.Server {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
